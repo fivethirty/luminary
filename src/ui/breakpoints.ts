ab367373399f9241b51725cf45e14be0484b8df8
Ben Phillips <bphillips7377@gmail.com>
2026-07-23 00:38:20 -0700

    Standardize breakpoints

---
+++ src/ui/breakpoints.ts
@@ -0,0 +1,12 @@
+export const VIEWPORT_BREAKPOINTS = {
+  mobile: '40rem',
+  tablet: '48rem',
+  laptop: '64rem',
+  extraLargeDesktop: '80rem',
+} as const;
+
+export type ViewportBreakpoint = keyof typeof VIEWPORT_BREAKPOINTS;
+
+export function viewportMaxWidthQuery(breakpoint: ViewportBreakpoint): string {
+  return `(max-width: ${VIEWPORT_BREAKPOINTS[breakpoint]})`;
+}
